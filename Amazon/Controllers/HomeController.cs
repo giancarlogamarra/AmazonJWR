a8c2c43d07ef9f4b33c84c4bb06bdf7c8bdf006d
Giancarlo Gamarra <ggamarra@solucionsmart.com>
2019-04-13 19:50:01 -0500

    Agregar archivos de proyecto.

---
+++ Amazon/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.Models;
+
+namespace Amazon.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RegBookForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RegBookForm(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                BookRepository.AddResponse(book);
+                return View("Thanks", book);
+            }
+            else {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ViewResult ListResponses()
+        { 
+          return View(BookRepository.Books);
+        }
+
+
+
+
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
